53f459a9f88549e5a95c0371b339a29bb4b3a466
Shouvick Aich <167152865+shouvickaich@users.noreply.github.com>
2024-07-09 19:03:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.40.109.48:9000/',
+  serverUrl: 'http://13.233.162.103:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
